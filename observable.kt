11d0c013adc8fa11a9da3457651b3ac8f07771ab
D-Soft-Tech <64334649+D-Soft-Tech@users.noreply.github.com>
2021-05-18 19:18:10 +0100

    Update observable.kt

--- observable.kt
+++ observable.kt
@@ -1,5 +1,20 @@
-private lateinit var input: ArrayList<Int>
-        
+package com.example.adebayorxjavaassignment
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.schedulers.Schedulers
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var input: ArrayList<Int>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
         val disposable = CompositeDisposable()
 
         for (i in 1..100) {
@@ -13,3 +28,5 @@ private lateinit var input: ArrayList<Int>
             .subscribe { it -> println(it) }
 
         disposable.add(result)
+    }
+}
